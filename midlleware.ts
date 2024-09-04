e2fe121cd1012c8a438af20aa0dda78c82865b53
Alex Pererodov <pererodovsasha2362000@gmail.com>
2024-09-04 04:37:22 +0300

    ооо

--- midlleware.ts
+++ midlleware.ts
@@ -2,12 +2,17 @@ import { NextResponse, type NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl;
+    console.log(`Middleware triggered for path: ${pathname}`); 
+
     const response = NextResponse.next();
 
-    if (pathname.startsWith('/blog')) {
-        response.headers.set('Cache-Control', 'public, max-age=120, s-maxage=240');
-    } else if (pathname.startsWith('/product')) {
+    if (pathname.startsWith('/tomorrow')) {
+        console.log('Setting Cache-Control for /tomorrow'); 
         response.headers.set('Cache-Control', 'public, max-age=60, s-maxage=120');
+    } else if (pathname.startsWith('/yesterday')) {
+        response.headers.set('Cache-Control', 'public, max-age=60, s-maxage=120');
+    } else if (pathname.startsWith('/today')) {
+        response.headers.set('Cache-Control', 'public, max-age=120, s-maxage=240');
     } else {
         response.headers.set('Cache-Control', 'public, max-age=30, s-maxage=60');
     }
