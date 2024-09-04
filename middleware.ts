2a7a1eac5342cd4c7feded8924c2ee76f42c3d55
Alex Pererodov <pererodovsasha2362000@gmail.com>
2024-09-04 07:20:58 +0300

    вв

--- middleware.ts
+++ middleware.ts
@@ -4,19 +4,19 @@ import { getRevalidate } from './lib/revalidateState';
 
 
 export async function middleware(req: NextRequest) {
-    const url = req.nextUrl.pathname;
+    // const url = req.nextUrl.pathname;
 
     
-    let ttl = 10; 
-    if (url.includes('tomorrow')) ttl = getRevalidate('tomorrow') || ttl;
-    if (url.includes('/')) ttl = getRevalidate('today') || ttl;
-    if (url.includes('yesterday')) ttl = getRevalidate('yesterday') || ttl;
+    // let ttl = 10; 
+    // if (url.includes('tomorrow')) ttl = getRevalidate('tomorrow') || ttl;
+    // if (url.includes('/')) ttl = getRevalidate('today') || ttl;
+    // if (url.includes('yesterday')) ttl = getRevalidate('yesterday') || ttl;
 
     
-    const response = NextResponse.next();
+    // const response = NextResponse.next();
 
     
-    response.headers.set('Cache-Control', `public, max-age=${ttl}, must-revalidate`);
+    // response.headers.set('Cache-Control', `public, max-age=${ttl}, must-revalidate`);
 
-    return response;
+    // return response;
 }
