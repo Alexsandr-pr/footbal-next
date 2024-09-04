23fdf59652935eae51c76181236e1c43228fadaa
Alex Pererodov <pererodovsasha2362000@gmail.com>
2024-09-04 05:01:06 +0300

    g

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,19 @@ const nextConfig = {
             },
         ],
     },
-    
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=8, must-revalidate'
+                    }
+                ],
+            },
+        ]
+    },
 };
 
 export default nextConfig;
