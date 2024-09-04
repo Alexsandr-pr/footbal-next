f8df83158d6973d99f90ab561f5eca5d49800a8c
Alex Pererodov <pererodovsasha2362000@gmail.com>
2024-09-04 03:15:30 +0300

    ке

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,43 +1,35 @@
 import { _SERVER_API } from "@/config/consts";
 import ClientRefresh from "./_components/ClientRefresh";
 import { LeaguesResponse } from "@/types/response";
-import { getRevalidate, setRevalidate } from "@/lib/revalidateState";
 
+export const revalidate = 8; 
 
-async function getDataMain(
-    path: string,
-): Promise<LeaguesResponse> {
-
-    console.log(`Fetching data from ${_SERVER_API}/games${path} at ${new Date().toISOString()}`);
-
-    const res = await fetch(`${_SERVER_API}/games${path}`, {
-        headers: {
-            'Cache-Control': `max-age=${getRevalidate("today")}`, 
-        }
+async function getDataMain(): Promise<LeaguesResponse> {
+    const res = await fetch(`${_SERVER_API}/games/today`, {
+        next: { revalidate }, 
     });
 
     if (!res.ok) {
         throw new Error("Failed to fetch data");
     }
 
-    console.log('Cache-Control:', res.headers.get('Cache-Control'));
-
     const data = await res.json();
-    if (data.TTL) {
-        setRevalidate('today', data.TTL);
-    }
-    const ttl = data.TTL;
-    return { leagues: data.leagues, calendar: data.calendar, ttl };
+
+    return {
+        leagues: data.leagues,
+        calendar: data.calendar,
+        ttl: data.TTL,
+    };
 }
 
 export default async function Page() {
 
-    const data = await getDataMain("/today");
+    const data = await getDataMain();
 
     return (
         <>  
-        <p>Last update: {new Date().toLocaleTimeString()}</p>
-        <ClientRefresh initialData={data} />
+            <p>Last update: {new Date().toLocaleTimeString()}</p>
+            <ClientRefresh initialData={data} />
         </>
     );
 }
